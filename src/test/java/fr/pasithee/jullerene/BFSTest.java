f4b78ca613cc6dbcb1bb05dced030a2711bb81e5
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2016-06-13 22:30:29 +0200

    bfs tests fixed. still need to think about the right way to do this, this is pretty ugly. should at least export conditions to something more readable, but now is time for bed. also, debatable whether the tests are actually at the right level.

--- src/test/java/fr/pasithee/jullerene/BFSTest.java
+++ src/test/java/fr/pasithee/jullerene/BFSTest.java
@@ -25,6 +25,7 @@ public class BFSTest {
         nodes = NodeFactory.createNodes(values);
 
         k33 = new EdgeListsGraph<>();
+        // bipartite between sets A = 0/1/2 and B = 3/4/5
         k33.addEdge(nodes.get(0), nodes.get(3));
         k33.addEdge(nodes.get(0), nodes.get(4));
         k33.addEdge(nodes.get(0), nodes.get(5));
@@ -36,6 +37,7 @@ public class BFSTest {
         k33.addEdge(nodes.get(2), nodes.get(5));
 
         disconnected = new EdgeListsGraph<>();
+        // two components path 0-1-2 / cycle 3-4-5
         disconnected.addEdge(nodes.get(0), nodes.get(1));
         disconnected.addEdge(nodes.get(1), nodes.get(2));
         disconnected.addEdge(nodes.get(3), nodes.get(4));
@@ -126,63 +128,54 @@ public class BFSTest {
         }
     }
 
-    /*@Test
+    @Test
     public void traversalWithK33ShouldWork() {
-        EdgeListsGraph g = new EdgeListsGraph(6);
-        g.addUndirectedEdge(0, 3);
-        g.addUndirectedEdge(0, 4);
-        g.addUndirectedEdge(0, 5);
-        g.addUndirectedEdge(1, 3);
-        g.addUndirectedEdge(1, 4);
-        g.addUndirectedEdge(1, 5);
-        g.addUndirectedEdge(2, 3);
-        g.addUndirectedEdge(2, 4);
-        g.addUndirectedEdge(2, 5);
-        Set<Integer> expectedFirstStep = new HashSet<>();
-        Set<Integer> expectedSecondStep = new HashSet<>();
-        Set<Integer> expectedThirdStep = new HashSet<>();
-        expectedFirstStep.add(0);
-        expectedSecondStep.add(3);
-        expectedSecondStep.add(4);
-        expectedSecondStep.add(5);
-        expectedThirdStep.add(1);
-        expectedThirdStep.add(2);
-        BFS bfs = new BFS(g);
-        int[] result = bfs.traversal();
-        Set<Integer> resultFirstStep = new HashSet<>();
-        Set<Integer> resultSecondStep = new HashSet<>();
-        Set<Integer> resultThirdStep = new HashSet<>();
-        for(int i = 0;i<6; i++) {
-            if(result[i] == 0) {
-                resultFirstStep.add(i);
-            } else if(result[i] <= 3) {
-                resultSecondStep.add(i);
-            } else {
-                resultThirdStep.add(i);
+        BFS<Node> bfs = new BFS<>(k33);
+        Map<Node, Integer> result = bfs.traversal();
+        int firstNodeIndex = -1;
+        for (Map.Entry<Node, Integer> kv : result.entrySet()) {
+            if (kv.getValue() == 0) {
+                firstNodeIndex = nodes.indexOf(kv.getKey());
+                break;
             }
         }
-        assertEquals(expectedFirstStep, resultFirstStep);
-        assertEquals(expectedSecondStep, resultSecondStep);
-        assertEquals(expectedThirdStep, resultThirdStep);
+
+        assertTrue(firstNodeIndex != -1);
+
+        if (firstNodeIndex < 3) {
+            assertTrue(firstNodeIndex == 0 || (result.get(nodes.get(0)) >= 4));
+            assertTrue(firstNodeIndex == 1 || (result.get(nodes.get(1)) >= 4));
+            assertTrue(firstNodeIndex == 2 || (result.get(nodes.get(2)) >= 4));
+            assertTrue(result.get(nodes.get(3)) > 0 && result.get(nodes.get(3)) < 4);
+            assertTrue(result.get(nodes.get(4)) > 0 && result.get(nodes.get(4)) < 4);
+            assertTrue(result.get(nodes.get(5)) > 0 && result.get(nodes.get(5)) < 4);
+        } else {
+            assertTrue(firstNodeIndex == 3 || (result.get(nodes.get(3)) >= 4));
+            assertTrue(firstNodeIndex == 4 || (result.get(nodes.get(4)) >= 4));
+            assertTrue(firstNodeIndex == 5 || (result.get(nodes.get(5)) >= 4));
+            assertTrue(result.get(nodes.get(0)) > 0 && result.get(nodes.get(0)) < 4);
+            assertTrue(result.get(nodes.get(1)) > 0 && result.get(nodes.get(1)) < 4);
+            assertTrue(result.get(nodes.get(2)) > 0 && result.get(nodes.get(2)) < 4);
+        }
     }
 
     @Test
     public void BFSWithDisconnectedGraphShouldWork() {
-        EdgeListsGraph g = new EdgeListsGraph(7);
-        g.addUndirectedEdge(0,1);
-        g.addUndirectedEdge(1,2);
-        g.addUndirectedEdge(2,6);
-        g.addUndirectedEdge(3,4);
-        g.addUndirectedEdge(3,5);
-        g.addUndirectedEdge(4, 5);
-        BFS bfs = new BFS(g);
-        int[] result = bfs.traversal();
-        assertEquals(0, result[0]);
-        assertEquals(1, result[1]);
-        assertEquals(2, result[2]);
-        assertEquals(3, result[6]);
-        assertEquals(4, result[3]);
-        assertTrue((result[4] == 5 && result[5] == 6) || (result[4] == 6 && result[5] == 5));
-    }*/
+        BFS<Node> bfs = new BFS<>(disconnected);
+        Map<Node, Integer> result = bfs.traversal();
+        if (result.get(nodes.get(0)) < 3) {
+            assertTrue(result.get(nodes.get(1)) < 3 && result.get(nodes.get(1)) >= 0);
+            assertTrue(result.get(nodes.get(2)) < 3 && result.get(nodes.get(2)) >= 0);
+            assertTrue(result.get(nodes.get(3)) >= 3);
+            assertTrue(result.get(nodes.get(4)) >= 3);
+            assertTrue(result.get(nodes.get(5)) >= 3);
+        } else {
+            assertTrue(result.get(nodes.get(1)) >= 3);
+            assertTrue(result.get(nodes.get(2)) >= 3);
+            assertTrue(result.get(nodes.get(3)) < 3 && result.get(nodes.get(3)) >= 0);
+            assertTrue(result.get(nodes.get(4)) < 3 && result.get(nodes.get(4)) >= 0);
+            assertTrue(result.get(nodes.get(5)) < 3 && result.get(nodes.get(5)) >= 0);
+        }
+    }
 
 }

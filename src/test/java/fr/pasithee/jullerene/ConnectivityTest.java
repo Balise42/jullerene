095ac676378a4c152b6579299a8d18389521d11e
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2016-06-18 22:22:10 +0200

    fixing connectivity tests

--- src/test/java/fr/pasithee/jullerene/ConnectivityTest.java
+++ src/test/java/fr/pasithee/jullerene/ConnectivityTest.java
@@ -3,10 +3,16 @@ package fr.pasithee.jullerene;
 import fr.pasithee.jullerene.algorithms.Connectivity;
 import fr.pasithee.jullerene.algorithms.Traversal;
 import fr.pasithee.jullerene.model.EdgeListsGraph;
+import fr.pasithee.jullerene.util.Node;
+import fr.pasithee.jullerene.util.NodeFactory;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -21,30 +27,31 @@ public class ConnectivityTest {
     @InjectMocks
     private Connectivity connectivity;
 
+    private List<Node> nodes;
+    private Map<Node, Integer> visited;
+
     @Before
     public void setup() {
         initMocks(this);
+        nodes = NodeFactory.createNodes(new String[]{"v0", "v1", "v2"});
+        visited = new HashMap<>();
+        when(traversal.step()).thenReturn(visited);
+        when(graph.getVertices()).thenReturn(nodes);
     }
 
-    /*@Test
+    @Test
     public void isConnectedWithConnectedArrayShouldReturnTrue() {
-        int[] visited = new int[3];
-        visited[0] = 0;
-        visited[1] = 1;
-        visited[2] = 2;
-        when(traversal.step()).thenReturn(visited);
-        when(graph.getNbVertices()).thenReturn(3);
+        visited.put(nodes.get(0), 0);
+        visited.put(nodes.get(1), 1);
+        visited.put(nodes.get(2), 2);
         assertTrue(connectivity.isConnected());
     }
 
     @Test
     public void isConnectedWithDisconnectedArrayShouldReturnFalse() {
-        int[] visited = new int[3];
-        visited[0] = 0;
-        visited[1] = -1;
-        visited[2] = 2;
-        when(traversal.step()).thenReturn(visited);
-        when(graph.getNbVertices()).thenReturn(3);
+        visited.put(nodes.get(0), 0);
+        visited.put(nodes.get(1), -1);
+        visited.put(nodes.get(2), 1);
         assertFalse(connectivity.isConnected());
-    }*/
+    }
 }

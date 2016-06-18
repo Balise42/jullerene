68fe5d56ce0b9da4e53588f10f93f918ceb940f3
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2016-06-18 22:29:32 +0200

    started fixing graph creation tests

--- src/test/java/fr/pasithee/jullerene/EdgeListGraphTest.java
+++ src/test/java/fr/pasithee/jullerene/EdgeListGraphTest.java
@@ -1,35 +1,40 @@
 package fr.pasithee.jullerene;
 
 import fr.pasithee.jullerene.model.EdgeListsGraph;
+import fr.pasithee.jullerene.util.Node;
+import fr.pasithee.jullerene.util.NodeFactory;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-/*public class GraphTest {
+public class EdgeListGraphTest {
     @Test
     public void hasEdgeWithUndirectedEdgesShouldReturnCorrectValues() {
-        EdgeListsGraph g = new EdgeListsGraph(4);
-        g.addUndirectedEdge(g.get(0), g.get(1));
-        g.addUndirectedEdge(g.get(0), g.get(2));
-        g.addUndirectedEdge(g.get(0), g.get(3));
+        EdgeListsGraph<Node> graph = new EdgeListsGraph<>();
+        List<Node> nodes =NodeFactory.createNodes(new String[]{"v0", "v1", "v2", "v3"});
+        graph.addEdge(nodes.get(0), nodes.get(1));
+        graph.addEdge(nodes.get(0), nodes.get(2));
+        graph.addEdge(nodes.get(0), nodes.get(3));
 
-        assertTrue(g.hasEdge(g.get(0), g.get(1)));
-        assertTrue(g.hasEdge(g.get(0), g.get(2)));
-        assertTrue(g.hasEdge(g.get(0), g.get(3)));
-        assertTrue(g.hasEdge(g.get(1), g.get(0)));
-        assertTrue(g.hasEdge(g.get(2), g.get(0)));
-        assertTrue(g.hasEdge(g.get(3), g.get(0)));
-        assertFalse(g.hasEdge(g.get(1), g.get(2)));
-        assertFalse(g.hasEdge(g.get(1), g.get(3)));
-        assertFalse(g.hasEdge(g.get(2), g.get(1)));
-        assertFalse(g.hasEdge(g.get(2), g.get(3)));
-        assertFalse(g.hasEdge(g.get(3), g.get(1)));
-        assertFalse(g.hasEdge(g.get(3), g.get(2)));
+        assertTrue(graph.hasEdge(nodes.get(0), nodes.get(1)));
+        assertTrue(graph.hasEdge(nodes.get(0), nodes.get(2)));
+        assertTrue(graph.hasEdge(nodes.get(0), nodes.get(3)));
+        assertTrue(graph.hasEdge(nodes.get(1), nodes.get(0)));
+        assertTrue(graph.hasEdge(nodes.get(2), nodes.get(0)));
+        assertTrue(graph.hasEdge(nodes.get(3), nodes.get(0)));
+        assertFalse(graph.hasEdge(nodes.get(1), nodes.get(2)));
+        assertFalse(graph.hasEdge(nodes.get(1), nodes.get(3)));
+        assertFalse(graph.hasEdge(nodes.get(2), nodes.get(1)));
+        assertFalse(graph.hasEdge(nodes.get(2), nodes.get(3)));
+        assertFalse(graph.hasEdge(nodes.get(3), nodes.get(1)));
+        assertFalse(graph.hasEdge(nodes.get(3), nodes.get(2)));
     }
 
-    @Test
+    /*@Test
     public void hasEdgeWithDirectedEdgesShouldReturnCorrectValues() {
         EdgeListsGraph g = new EdgeListsGraph(4);
         g.addDirectedEdge(g.get(0), g.get(1));
@@ -116,8 +121,4 @@ import static org.junit.Assert.assertTrue;
         g.addDirectedEdge(0,0);
     }*/
 
-    public class GraphTest {
-        @Test
-        public void doNothing() {
-        }
-    }
+}
